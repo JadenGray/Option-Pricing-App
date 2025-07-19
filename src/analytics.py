81317b0af5c15094b9738073f6d766cc4c4e116e
Jaden <jaden.silver.gray@gmail.com>
2025-07-19 17:19:29 +0100

    Structure Change
    Strategy Development (looks profitable), pnl not yet being calculated correctly though.

--- src/analytics.py
+++
@@ -1,9 +0,0 @@
-def compute_pnl(df):
-    # sort by contract and timestamp
-    df = df.sort_values(['option_symbol','datetime'])
-    df['next_exec_price'] = df.groupby('option_symbol')['exec_price'].shift(-1)
-    # PnL = (next_exec_price - exec_price) * position * 100
-    df['pnl'] = (df['next_exec_price'] - df['exec_price']) * df['signal'] * 100
-    daily = df.groupby(df['datetime'].dt.date)['pnl'].sum().reset_index(name='daily_pnl')
-    daily['cumulative_pnl'] = daily['daily_pnl'].cumsum()
-    return daily
